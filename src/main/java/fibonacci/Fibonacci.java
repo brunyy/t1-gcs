934fdb64123439b5c749a0d47d83f8d6590d2b79
wnyyyy <viniberizzi@gmail.com>
2022-09-22 13:36:52 -0300

    Add fibonacci code

--- src/main/java/fibonacci/Fibonacci.java
+++ src/main/java/fibonacci/Fibonacci.java
@@ -1,7 +1,21 @@
 package fibonacci;
 
+import java.util.Arrays;
+
 public class Fibonacci {
     public static void main(String[] args) {
-        System.out.println("Hello, bee!");
+        int a = 1;
+        int b = 0;
+        int n = 10;
+
+        int[] fib = new int[n+1];
+
+        for (int i = 1; i < n+1; i++ ) {
+            fib[i] = a;
+            a = a + b;
+            b = a - b;
+        }
+
+        System.out.println(Arrays.toString(fib));
     }
 }
